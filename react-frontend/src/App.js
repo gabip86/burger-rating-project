f34b1be75576d641ffe688deb1b632fb1712cc01
Gabi <gabee_88@yahoo.com>
2021-11-11 14:56:01 +0100

    feat: created BurgerPlace component

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,10 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import BurgerPlace from "./components/BurgerPlace";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <BurgerPlace />
+        <BurgerPlace />
+      </header>
     </div>
   );
 }
---
+++ react-frontend/src/components/BurgerPlace.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BurgerPlace extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello from the BurgerPlace component</p>
+      </div>
+    )
+  }
+}
