24f23077e6146a12e3dc3ca3e0f1bcf8a6eec019
Gabi <gabee_88@yahoo.com>
2021-11-13 16:21:52 +0100

    chore: fix issues

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,13 +1,10 @@
+import logo from "./logo.svg";
 import "./App.css";
-import BurgerPlace from "./components/BurgerPlace";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <BurgerPlace />
-        <BurgerPlace />
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
