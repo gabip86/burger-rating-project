8927e3daf52b7f5a16707a9a7b7e03e2ec812c0a
Gabi <gabee_88@yahoo.com>
2021-11-17 11:34:59 +0100

    feat: added delete api

--- springboot-backend/src/main/java/com/burger/springbootbackend/controller/PlaceController.java
+++ springboot-backend/src/main/java/com/burger/springbootbackend/controller/PlaceController.java
@@ -1,15 +1,20 @@
 package com.burger.springbootbackend.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.burger.springbootbackend.exception.ResourceNotFoundException;
 import com.burger.springbootbackend.model.Place;
 import com.burger.springbootbackend.repository.PlaceRepository;
 
@@ -30,5 +35,16 @@ public class PlaceController {
 	@PostMapping("/posts")
 	public Place cratePlace(@RequestBody Place place) {
 		return placeRepository.save(place);
+	}
+	
+	@DeleteMapping("/post/{id}")
+	public Map<String, Boolean> deletePlace(@PathVariable(value="id") Long userId) throws Exception {
+		Place place = placeRepository
+				.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found on :: " + userId));
+		placeRepository.delete(place);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return response;
 	}
 }
