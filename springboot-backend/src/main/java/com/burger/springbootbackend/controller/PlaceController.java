93be421773c13027d37f3009d51f10a1b366c400
Gabi <gabee_88@yahoo.com>
2021-11-17 11:23:17 +0100

    feat: added post api

--- springboot-backend/src/main/java/com/burger/springbootbackend/controller/PlaceController.java
+++ springboot-backend/src/main/java/com/burger/springbootbackend/controller/PlaceController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,10 @@ public class PlaceController {
 	@GetMapping("/posts")
 	public List<Place> getAllPlaces() {
 		return placeRepository.findAll();
+	}
+	
+	@PostMapping("/posts")
+	public Place cratePlace(@RequestBody Place place) {
+		return placeRepository.save(place);
 	}
 }
