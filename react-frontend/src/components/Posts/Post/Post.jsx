058c535d605b0beb118c909145107025dcefc5b7
Gabi <gabee_88@yahoo.com>
2021-11-17 13:49:56 +0100

    feat: added timestamp to db and ui

--- react-frontend/src/components/Posts/Post/Post.jsx
+++ react-frontend/src/components/Posts/Post/Post.jsx
@@ -29,9 +29,9 @@ const Post = ({ post, setCurrentId }) => {
       />
       <div className={classes.overlay}>
         <Typography variant="h6">{post.name}</Typography>
-        {/* <Typography variant="body2">
-          {moment(post.createdAt).fromNow()}
-        </Typography> */}
+        <Typography variant="body2">
+          {moment(post.created).fromNow()}
+        </Typography>
       </div>
       <div className={classes.overlay2}>
         <Button
